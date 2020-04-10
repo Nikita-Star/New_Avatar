30fcc6add9f37a53d54af7f86fd73bc95876e626
Nikita-Star <37474425+Nikita-Star@users.noreply.github.com>
2020-04-10 19:42:47 +0300

    11

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from utils import *
 import pytz
 import time
 client = TelegramClient('банан', api_id, api_hash)
-
-client.start()
-prev_update_time = ""
-
-while True:
+try:
+    client.start()
+    prev_update_time = ""
+ 
+    while True:
     
-    if time_has_changed(prev_update_time):
-        prev_update_time = convert_time_to_string(dt.datetime.now(pytz.timezone('Europe/Moscow')))
-        prev_update_time = prev_update_time.replace(':','_')
+        if time_has_changed(prev_update_time):
+            prev_update_time = convert_time_to_string(dt.datetime.now(pytz.timezone('Europe/Moscow')))
+            prev_update_time = prev_update_time.replace(':','_')
 
-        client(DeletePhotosRequest(client.get_profile_photos('me')))
-        file = client.upload_file(f"time_images/{prev_update_time}.jpg")
-        client(UploadProfilePhotoRequest(file))
-        time.sleep(15)
+            client(DeletePhotosRequest(client.get_profile_photos('me')))
+            file = client.upload_file(f"time_images/{prev_update_time}.jpg")
+            client(UploadProfilePhotoRequest(file))
+            time.sleep(15)
+except:
+    print ('error' + dt.datetime.now())
 
 
             
